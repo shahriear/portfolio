d53aaa293d692248790566b7430b5e5c1f31a238
shuvo pc <shahriearshuvo@gmail.com>
2026-01-05 12:21:31 +0600

    homee

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,7 +9,7 @@ import { useRouter, usePathname } from "next/navigation";
 import SearchPopover from "@/components/SearchPopover";
 
 const links = [
-  { href: "/", label: "Homee" },
+  { href: "/", label: "Home" },
   { href: "/about", label: "About" },
   { href: "/resume", label: "Resume" },
   { href: "/portfolio", label: "Portfolio" },
